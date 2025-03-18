9e3f1f0b9ab8d03f3449fb4f7f3776863242ef22
jlaxamana1298 <jlaxamana1298@gmail.com>
2025-03-17 17:16:54 -0700

    Assignment 1 submission 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# initialize arguments
+if [ "$#" -ne 2 ]; then 
+    echo "All parameters not specified correctly"
+    exit 1
+fi
+
+filesdir="$1"
+searchstr="$2"
+
+# check if filesdir is dir
+if [ ! -d "$filesdir" ]; then
+    echo "Provided filesdir is not valid dir"
+    exit 1
+fi 
+
+# get numfiles
+numfiles=$(grep "$searchstr" -r -l "$filesdir" | wc -l)
+# get numlines
+numlines=$(grep "$searchstr" -s -r "$filesdir" | wc -l)
+
+# print output
+echo "The number of files are $numfiles and the number of matching lines are $numlines"
+
+exit 0
---
+++ finder-app/writer.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# initialize arguments
+if [ "$#" -ne 2 ]; then
+    echo "All parameters not specified correctly"
+    exit 1
+fi
+
+writefile="$1"
+writestr="$2"
+
+# check if directory exists
+writedir=$(dirname "$writefile")
+if [ ! -d "$writedir" ]; then
+    mkdir -p "$writedir" 2>/dev/null
+fi
+if [ $? -ne 0 ]; then
+    echo "Error: could not create directory"
+    exit 1
+fi
+
+# Write to file
+echo "$writestr" > "$writefile" 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Error: failed to write to file"
+    exit 1
+fi
+
+echo "Successfuly wrote file"
+exit 0
